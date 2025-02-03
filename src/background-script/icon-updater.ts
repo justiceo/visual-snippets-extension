1cfdaaf4157c67fada2ad66f28e1c085368482a0
Justice Ogbonna <justiceo@users.noreply.github.com>
2025-02-03 05:16:06 -0800

    [TAG][Submitted] Custom xtension template for Screenshot

--- src/background-script/icon-updater.ts
+++ src/background-script/icon-updater.ts
@@ -13,13 +13,13 @@ chrome.tabs.onActivated.addListener((activeInfo) => {
 // Automatically disable icon on webstore and new tab pages.
 // FYI: window.alert and window.confirm are now disabled in MV3.
 const updateIcon = (url?: string) => {
-  const icon =
-    !url ||
-    !url.trim() ||
-    url.startsWith("chrome-extension://") ||
-    url.startsWith("chrome://newtab/") ||
-    url.startsWith("https://chrome.google.com/webstore")
-      ? "assets/logo-gray-128x128.png"
-      : "assets/logo-128x128.png";
-  chrome.action.setIcon({ path: chrome.runtime.getURL(icon) });
+  // const icon =
+  //   !url ||
+  //   !url.trim() ||
+  //   url.startsWith("chrome-extension://") ||
+  //   url.startsWith("chrome://newtab/") ||
+  //   url.startsWith("https://chrome.google.com/webstore")
+  //     ? "assets/logo-gray-128x128.png"
+  //     : "assets/logo-128x128.png";
+  // chrome.action.setIcon({ path: chrome.runtime.getURL(icon) });
 };
--- src/const.ts
+++ src/const.ts
@@ -1,12 +1,12 @@
 // No deps. Treat as json file.
 
-export const packageName = "xtension";
+export const packageName = "screenshot";
 export const extensionId = chrome?.i18n?.getMessage("@@extension_id");
 export const applicationId = packageName + "/" + extensionId;
 export const sentryDsn =
   "https://b1d81a9e5f1546f79885a473ce33128c@o526305.ingest.sentry.io/6244539";
-export const measurementId = "G-JWLV6CJVSJ";
-export const gaApiSecret = "E2EWHH--QbSaf9-f0ePC5g";
-export const uninstallUrl = "https://forms.gle/AUoB1eLxRjN8j3xM9";
+export const measurementId = "G-19Y7T52NBH";
+export const gaApiSecret = "Cu0vRk7eTX21s66ymUrdIQ";
+export const uninstallUrl = "https://forms.gle/oXHY8SXFMXN37SG69";
 export const storeLink =
   "https://chrome.google.com/webstore/detail/" + extensionId;
