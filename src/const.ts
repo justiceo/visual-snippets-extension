9abd68982e08e742928215277f559ee600cde57f
Justice Ogbonna <justiceo@users.noreply.github.com>
2025-03-01 21:39:09 -0800

    Remove dependency on sentry

--- src/const.ts
+++ src/const.ts
@@ -3,8 +3,6 @@
 export const packageName = "screenshot";
 export const extensionId = chrome?.i18n?.getMessage("@@extension_id");
 export const applicationId = packageName + "/" + extensionId;
-export const sentryDsn =
-  "https://b1d81a9e5f1546f79885a473ce33128c@o526305.ingest.sentry.io/6244539";
 export const measurementId = "G-19Y7T52NBH";
 export const gaApiSecret = "Cu0vRk7eTX21s66ymUrdIQ";
 export const uninstallUrl = "https://forms.gle/oXHY8SXFMXN37SG69";
