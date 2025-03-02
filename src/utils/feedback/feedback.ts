441a4e4d77bd378433798465e9dfe0ab6ceeceff
Justice Ogbonna <justiceo@users.noreply.github.com>
2025-03-01 21:50:58 -0800

    Fix i18n issue

--- src/utils/feedback/feedback.ts
+++ src/utils/feedback/feedback.ts
@@ -66,7 +66,7 @@ class FeedbackForm extends HTMLElement {
     shadow.append(style, documentFragment);
 
     const size = elem.getAttribute("size") ?? "inline";
-    const appName = elem.getAttribute("app-name") ?? i18n("@appName");
+    const appName = elem.getAttribute("app-name") ?? i18n("const_appName");
     const logo =
       elem.getAttribute("logo-url") ??
       chrome.runtime.getURL("assets/logo-24x24.png");
